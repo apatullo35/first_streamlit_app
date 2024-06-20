38a091c55e9b153c3eceaa5dfa0baced1465d7eb
apatullo35 <apatullo35@gmail.com>
2024-06-20 12:41:04 -0400

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,28 +2,30 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+st.title('Uber pickups in NYC')
 
-df
+# df = pd.DataFrame({
+#   'first column': [1, 2, 3, 4],
+#   'second column': [10, 20, 30, 40]
+# })
 
-chart_data = pd.DataFrame(
-  np.random.randn(20, 3),
-  columns=['a', 'b', 'c'])
+# df
 
-st.line_chart(chart_data)
+# chart_data = pd.DataFrame(
+#   np.random.randn(20, 3),
+#   columns=['a', 'b', 'c'])
 
-map_data = pd.DataFrame(
-  np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-  columns=['lat', 'lon'])
+# st.line_chart(chart_data)
 
-st.map(map_data)
+# map_data = pd.DataFrame(
+#   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+#   columns=['lat', 'lon'])
 
-x = st.slider('x') #
-st.write(x, 'squared is', x * x)
+# st.map(map_data)
 
-st.text_input("Your name", key="name")
+# x = st.slider('x') #
+# st.write(x, 'squared is', x * x)
 
-st.session_state.name
+# st.text_input("Your name", key="name")
+
+# st.session_state.name
