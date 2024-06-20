edb6928b1a81cb21659d540e216998927dda84b0
apatullo35 <apatullo35@gmail.com>
2024-06-20 12:12:20 -0400

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -1,9 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+# df = pd.DataFrame({
+#   'first column': [1, 2, 3, 4],
+#   'second column': [10, 20, 30, 40]
+# })
 
-df
+# df
+
+chart_data = pd.DataFrame(
+  np.random.randn(20,3),
+  columns=['a', 'b', 'c'])
+
+st.line_chard(chart_data)
