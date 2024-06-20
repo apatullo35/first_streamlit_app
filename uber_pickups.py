8be57d79d48d4db1090f8b4b6bcd46a4dba1af0d
apatullo35 <apatullo35@gmail.com>
2024-06-20 12:17:24 -0400

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -9,8 +9,14 @@ import pandas as pd
 
 # df
 
-chart_data = pd.DataFrame(
-  np.random.randn(20, 3),
-  columns=['a', 'b', 'c'])
+# chart_data = pd.DataFrame(
+#   np.random.randn(20, 3),
+#   columns=['a', 'b', 'c'])
 
-st.line_chart(chart_data)
+# st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+  np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+  columns=['lat', 'lon'])
+
+st.map(map_data)
